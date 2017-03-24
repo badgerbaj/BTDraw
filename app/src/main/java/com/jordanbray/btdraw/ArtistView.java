0314185ce808acffda4ac3332e93142e59513070
Todd <Toddab16@gmail.com>
2017-03-24 15:56:54 -0400

    Allows for the drawing of rectangles and ovals

--- app/src/main/java/com/jordanbray/btdraw/ArtistView.java
+++ app/src/main/java/com/jordanbray/btdraw/ArtistView.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,12 +17,15 @@ import android.view.View;
 
 public class ArtistView extends View {
 
+    private int mode = 0;
     private Path path;
     private Paint paint, canvasPaint;
     private int paintColor = 0xFF000000;
     private int canvasColor = 0xFFFFFFFF;
     private Canvas canvas;
     private Bitmap canvasBitmap;
+    PointF upperleft = new PointF(0,0);
+    PointF lowerright = new PointF(0,0);
 
 
     public ArtistView(Context context) {
@@ -63,12 +68,14 @@ public class ArtistView extends View {
 
         canvasBitmap = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(canvasBitmap);
+        canvas.drawColor(canvasColor);
+
     }
 
     @Override
     protected void onDraw(Canvas c) {
-        c.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-        c.drawPath(path, paint);
+            c.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+            c.drawPath(path, paint);
     }
 
     @Override
@@ -77,12 +84,49 @@ public class ArtistView extends View {
         float y = event.getY();
 
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            path.moveTo(x, y);
+            if (mode == 0) {
+                path.moveTo(x, y);
+            }
+            if (mode == 1) {
+                upperleft.x = x;
+                upperleft.y = y;
+                lowerright.x = x;
+                lowerright.y = y;
+            }
+            if (mode == 2) {
+                upperleft.x = x;
+                upperleft.y = y;
+                lowerright.x = x;
+                lowerright.y = y;
+            }
+
         } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            path.lineTo(x, y);
+            if (mode == 0) {
+                path.lineTo(x, y);
+            }
+            if (mode == 1) {
+                lowerright.x = x;
+                lowerright.y = y;
+            }
+            if (mode == 2) {
+                lowerright.x = x;
+                lowerright.y = y;
+            }
+
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
-            canvas.drawPath(path, paint);
-            path.reset();
+            if (mode == 0) {
+                canvas.drawPath(path, paint);
+                path.reset();
+            }
+            if (mode == 1) {
+                canvas.drawRect(upperleft.x,  upperleft.y, lowerright.x, lowerright.y, paint);
+                invalidate();
+            }
+            if (mode == 2) {
+                RectF rec = new RectF(upperleft.x,  upperleft.y, lowerright.x, lowerright.y);
+                canvas.drawOval(rec, paint);
+                invalidate();
+            }
         }
         else {
             return false;
@@ -90,6 +134,10 @@ public class ArtistView extends View {
 
         invalidate();
         return true;
+    }
+
+    public void setMode (int i) {
+        mode = i;
     }
 
     public void setPaintColor (int i) {
@@ -103,5 +151,11 @@ public class ArtistView extends View {
         paintColor = canvasColor;
         paint.setColor(paintColor);
     }
+
+    public void setCanvasColor () {
+        canvas.drawColor(canvasColor);
+    }
+
+
 
 }
